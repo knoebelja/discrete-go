c5ea825f88c996a65b0c978ecfdf66402a7c74b0
Joseph Knoebel <knoebelja@gmail.com>
2020-11-30 11:16:37 -0500

    update some minor stuff

--- check.go
+++ check.go
@@ -28,18 +28,18 @@ func check(ok bool, msg string) (err error) {
 	return
 }
 
-func checkPositiveInteger(i int64) (err error) {
-	return check(isPositiveInteger(i), fmt.Sprintf("%d must be greater than 0", i))
+func checkPositiveInteger(errwhen string, i int64) (err error) {
+	return check(isPositiveInteger(i), fmt.Sprintf("%s: %d must be greater than 0", errwhen, i))
 }
 
-func checkNegativeInteger(i int64) (err error) {
-	return check(isNegativeInteger(i), fmt.Sprintf("%d must be less than 0", i))
+func checkNegativeInteger(errwhen string, i int64) (err error) {
+	return check(isNegativeInteger(i), fmt.Sprintf("%s: %d must be less than 0", errwhen, i))
 }
 
-func checkNonnegativeInteger(i int64) (err error) {
-	return check(isNonnegativeInteger(i), fmt.Sprintf("%d must be less than or equal to 0", i))
+func checkNonnegativeInteger(errwhen string, i int64) (err error) {
+	return check(isNonnegativeInteger(i), fmt.Sprintf("%s: %d must be less than or equal to 0", errwhen, i))
 }
 
-func checkNonpositiveInteger(i int64) (err error) {
-	return check(isNonpositiveInteger(i), fmt.Sprintf("%d must be greater than or equal to 0", i))
+func checkNonpositiveInteger(errwhen string, i int64) (err error) {
+	return check(isNonpositiveInteger(i), fmt.Sprintf("%s: %d must be greater than or equal to 0", errwhen, i))
 }
